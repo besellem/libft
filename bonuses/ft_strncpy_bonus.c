c7e69e34eae31016fd0e694196a4da478d436fb9
bsellem <[bensellem3@gmail.com]>
2020-09-13 18:19:12 +0200

    strcpy* functions added

---
+++ bonuses/ft_strncpy_bonus.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/13 18:13:58 by besellem          #+#    #+#             */
+/*   Updated: 2020/09/13 18:17:17 by besellem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (src[i] && i < len)
+	{
+		dst[i] = src[i];
+		++i;
+	}
+	while (i < len)
+	{
+		dst[i] = '\0';
+		++i;
+	}
+	return (dst);
+}
