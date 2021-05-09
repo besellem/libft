a8ac75bed8413af13dc2fe12497e70574af99e89
Benjamin Sellem <69735857+besellem@users.noreply.github.com>
2021-05-10 01:01:46 +0200

    cleaned strlcpy

--- srcs/string/ft_strlcpy.c
+++ srcs/string/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 02:29:35 by besellem          #+#    #+#             */
-/*   Updated: 2021/05/09 18:59:14 by besellem         ###   ########.fr       */
+/*   Updated: 2021/05/10 00:46:23 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,18 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	len;
-	size_t	i;
 
 	if (!src)
 		return (0);
-	len = 0;
-	while (src[len])
-		++len;
+	len = ft_strlen(src);
 	if (dstsize == 0)
 		return (len);
-	i = 0;
-	while (src[i] && i < (dstsize - 1))
+	if (len >= (dstsize - 1))
 	{
-		dst[i] = src[i];
-		++i;
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
 	}
-	dst[i] = '\0';
+	else
+		ft_memcpy(dst, src, len + 1);
 	return (len);
 }
-
-// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-// {
-// 	const size_t	_sz = ft_strlen(src);
-
-// 	if (_sz + 1 > dstsize)
-// 		ft_memcpy(dst, src, _sz + 1);
-// 	else if (dstsize > 0)
-// 	{
-// 		ft_memcpy(dst, src, dstsize - 1);
-// 		dst[dstsize - 1] = '\0';
-// 	}
-// 	return (_sz);
-// }
