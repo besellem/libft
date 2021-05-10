0671e26e7760341fb728b4c910ce1e4c4f912f33
Benjamin Sellem <69735857+besellem@users.noreply.github.com>
2021-05-10 02:58:18 +0200

    memmove big opti

--- srcs/string/ft_memmove.c
+++ srcs/string/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 02:29:24 by besellem          #+#    #+#             */
-/*   Updated: 2021/04/19 11:46:04 by besellem         ###   ########.fr       */
+/*   Updated: 2021/05/10 02:57:17 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*s1;
-	unsigned char	*s2;
-	size_t			i;
+	const size_t	l_size = sizeof(unsigned long);
+	unsigned long	*l_src;
+	unsigned long	*l_dst;
+	unsigned char	*c_src;
+	unsigned char	*c_dst;
 
-	s1 = (unsigned char *)dst;
-	s2 = (unsigned char *)src;
 	if (dst == src)
 		return (dst);
 	if (dst > src)
 	{
+		l_src = (unsigned long *)(src + len);
+		l_dst = (unsigned long *)(dst + len);
+		while (len >= l_size)
+		{
+			*--l_dst = *--l_src;
+			len -= l_size;
+		}
+		c_src = (unsigned char *)l_src;
+		c_dst = (unsigned char *)l_dst;
 		while (len-- > 0)
-			s1[len] = s2[len];
+			*--c_dst = *--c_src;
 	}
 	else
-	{
-		i = 0;
-		while (i < len)
-		{
-			s1[i] = s2[i];
-			++i;
-		}
-	}
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
