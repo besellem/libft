bfdd886f3388edc5ab7a55f4ce000b92b92d29bb
Benjamin Sellem <69735857+besellem@users.noreply.github.com>
2021-09-01 03:26:48 +0200

    strlen opti

--- incs/ft_stdint.h
+++ incs/ft_stdint.h
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 13:42:44 by besellem          #+#    #+#             */
-/*   Updated: 2021/08/26 00:29:58 by besellem         ###   ########.fr       */
+/*   Updated: 2021/09/01 03:21:10 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 typedef signed char				t_int8;
 typedef short					t_int16;
 typedef int						t_int32;
-typedef long long int			t_int64;
+typedef long long				t_int64;
 
 typedef unsigned char			t_uint8;
 typedef unsigned short			t_uint16;
 typedef unsigned int			t_uint32;
-typedef unsigned long long int	t_uint64;
+typedef unsigned long long		t_uint64;
 
 # else
 
@@ -39,16 +39,16 @@ typedef uint16_t				t_uint16;
 typedef uint32_t				t_uint32;
 typedef uint64_t				t_uint64;
 
-# endif
+# endif /* !defined(__CLANG_STDINT_H) */
 
 /*
-** t_wide_int__ is a 16 bytes type, wider than an unsigned long long that is
+** __int128_t is a 16 bytes type, wider than an `unsigned long long', it is
 ** defined by gcc or clang on certain versions.
 */
 # if defined(__SIZEOF_INT128__)
 typedef __int128_t				t_wide_int__;
 # else
 typedef unsigned long long		t_wide_int__;
-# endif
+# endif /* defined(__SIZEOF_INT128__) */
 
 #endif
--- srcs/string/ft_strlen.c
+++ srcs/string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 02:29:36 by besellem          #+#    #+#             */
-/*   Updated: 2021/08/24 01:05:47 by besellem         ###   ########.fr       */
+/*   Updated: 2021/09/01 03:18:18 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,61 +15,29 @@
 #include <limits.h>
 
 /*
-** 1GB in ~500 ms (the real one does it in ~100 ms)
+** 1GB in ~280 ms (the real one does it in ~100 ms)
 */
 size_t	ft_strlen(const char *s)
 {
-	char	*cpy;
+	const size_t	ones = (SIZE_T_MAX / UCHAR_MAX);
+	const size_t	highs = ones * (UCHAR_MAX / 2 + 1);
+	size_t			*l_str;
+	char			*c_str;
 
 	if (LIBC_NO_CRASH_ON_NULL && !s)
 		return (0);
-	cpy = (char *)s;
-	while (*cpy)
+	c_str = (char *)s;
+	while ((uintptr_t)c_str % sizeof(size_t))
 	{
-		if (!cpy[1])
-			return (cpy - s + 1);
-		if (!cpy[2])
-			return (cpy - s + 2);
-		if (!cpy[3])
-			return (cpy - s + 3);
-		if (!cpy[4])
-			return (cpy - s + 4);
-		if (!cpy[5])
-			return (cpy - s + 5);
-		if (!cpy[6])
-			return (cpy - s + 6);
-		if (!cpy[7])
-			return (cpy - s + 7);
-		cpy += 8;
+		if (!*c_str)
+			return (c_str - s);
+		++c_str;
 	}
-	return (cpy - s);
+	l_str = (size_t *)c_str;
+	while (!((*l_str) - ones & ~(*l_str) & highs))
+		++l_str;
+	c_str = (char *)l_str;
+	while (*c_str)
+		++c_str;
+	return ((size_t)c_str - (size_t)s);
 }
-
-/*
-** THE BEST I CAME UP WITH (but segfault with -fsanitize=address in some cases)
-** 1GB in ~280 ms (the real one does it in ~100 ms)
-*/
-// size_t	ft_strlen(const char *s)
-// {
-// 	const size_t	ones = (SIZE_T_MAX / UCHAR_MAX);
-// 	const size_t	highs = ones * (UCHAR_MAX / 2 + 1);
-// 	size_t			*l_str;
-// 	char			*c_str;
-
-// 	if (LIBC_NO_CRASH_ON_NULL && !s)
-// 		return (0);
-// 	c_str = (char *)s;
-// 	while ((uintptr_t)c_str % sizeof(size_t))
-// 	{
-// 		if (!*c_str)
-// 			return (c_str - s);
-// 		++c_str;
-// 	}
-// 	l_str = (size_t *)c_str;
-// 	while (!((*l_str) - ones & ~(*l_str) & highs))
-// 		++l_str;
-// 	c_str = (char *)l_str;
-// 	while (*c_str)
-// 		++c_str;
-// 	return ((size_t)c_str - (size_t)s);
-// }
